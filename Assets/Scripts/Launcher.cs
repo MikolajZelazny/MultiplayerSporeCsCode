96f36780c03808711899e7a6b95a141561e3e6d2
MikolajZelazny <MikolajZelazny.it@gmail.com>
2021-01-24 00:51:04 +0100

    Files check
    Files check

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Launcher : MonoBehaviourPunCallback
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster(); 
+    {
+        //base.OnConnectedtoMaster();
+        PhotonNetwork.JoinLobby();
+    }
+
+
+// Update is called once per frame
+void Update()
+{
+
+}
+}
