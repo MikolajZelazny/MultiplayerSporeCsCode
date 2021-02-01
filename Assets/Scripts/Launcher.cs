6df91474e5db1e45dbbece9a859934fcc0f25388
MikolajZelazny <MikolajZelazny.it@gmail.com>
2021-02-02 00:02:31 +0100

    room menu orientation
    room menu orientation

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using TMPro;
 
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+
+    [SerializeField] TMP_InputField roomNameInputField;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +25,7 @@ public class Launcher : MonoBehaviourPunCallbacks
         PhotonNetwork.JoinLobby();
     }
 
-public override void OnJoinedLobby()
+    public override void OnJoinedLobby()
 {
         MenuManager.Instance.OpenMenu("title");
         Debug.Log("Joined Lobby");
@@ -29,9 +33,24 @@ public override void OnJoinedLobby()
 
 
 // Update is called once per frame
-void Update()
+    public void CreateRoom()
 {
-
+        if (string.IsNullOrEmpty(roomNameInputField.text))
+        {
+            return;
+        }
+        PhotonNetwork.CreateRoom(roomNameInputField.text);
+        MenuManager.Instance.OpenMenu("loading");
 }
+    public override void OnJoinedRoom()
+    {
+        //base.OnJoinedRoom();
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        //base.OnCreateRoomFailed(returnCode, message);
+    }
+
 
 }
